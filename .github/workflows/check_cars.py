e495e951ed6f6d7b0f26cfedfad23d891185ca7e
kagaribikaimitu-dotcom <kagaribikai.mitu@gmail.com>
2025-12-15 15:48:42 +0900

    Remove parentheses from URL strings in check_cars.py

--- .github/workflows/check_cars.py
+++ .github/workflows/check_cars.py
@@ -5,9 +5,9 @@ from bs4 import BeautifulSoup
 WEBHOOK = os.environ.get("DISCORD_WEBHOOK_URL")
 
 URLS = {
-    "カーセンサー": "（https://www.carsensor.net/usedcar/search.php?CARC=SB_S052&GRDKC=SB_S052_F001_K018%2ASB_S052_F001_K022&OPTCD=REP0&YMIN=2017&SMAX=100000&CL=GL&AL=1&SORT=19&STID=SMPH0001）",
-    "グーネット": "（https://www.goo-net.com/php/search/summary.php）",
-    "スグダス": "（https://ucar.subaru.jp/php/search/summary.php?baitai=iphone）",
+    "カーセンサー": "https://www.carsensor.net/usedcar/search.php?CARC=SB_S052&GRDKC=SB_S052_F001_K018%2ASB_S052_F001_K022&OPTCD=REP0&YMIN=2017&SMAX=100000&CL=GL&AL=1&SORT=19&STID=SMPH0001",
+    "グーネット": "https://www.goo-net.com/php/search/summary.php",
+    "スグダス": "https://ucar.subaru.jp/php/search/summary.php?baitai=iphone",
 }
 
 def notify(message):
